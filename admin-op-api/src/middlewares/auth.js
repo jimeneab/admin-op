adcf81cfbc33c14fc6a6d56b5d803d6112216809
jimeneab <abraham.tal.ji@gmail.com>
2021-09-27 08:59:21 -0500

    encrypted password

--- admin-op-api/src/middlewares/auth.js
+++ admin-op-api/src/middlewares/auth.js
@@ -11,18 +11,21 @@ module.exports = {
         
         if(!userFound) return false
 
-        if(userFound.password != password) return false
+        const isPasswordValid = await bcrypt.compare(password, userFound.password)
+
+        if(!isPasswordValid) return false
         
         return true
     },
 
     async signUp(data){
         let { name, email, role, password } = data
+        let encryptedPassword = await bcrypt.hash(password, 10)
         let newUser = await users.create({
             name,
             email,
             role,
-            password
+            password: encryptedPassword
         })
         return newUser
     }
